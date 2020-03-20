fae74935e798c1b2f2cfda00865958e91bebef21
Michael Araque <32944677+michydev@users.noreply.github.com>
2020-03-20 21:38:01 +0100

    Update coronavirus.php
    Se actualiza el código casi en su totalidad debido a la dificultad de parseamiento del PDF que provee el Ministerio de Sanidad

--- corona_classes/coronavirus.php
+++ corona_classes/coronavirus.php
@@ -9,77 +9,61 @@ class Coronavirus {
         $parser = new \Smalot\PdfParser\Parser();
         $pdf = $parser->parseFile('data.pdf');
         $text = $pdf->getText();
-        $text = preg_replace('/	 	/', ' ', $text);
-        $text = explode('Ingresados en', $text);
-        $text = explode('Este inform', $text[1]);
-        $text = preg_replace(['/\(L\)/', '/Fallecidos	  /', '/UCI  /', '/	2 	/'], ['', '', '', ' '], $text[0]);
-        $text = ltrim($text);
-        $this->comunidades_autonomas = explode('CCAA Total casos Fallecidos ', $text);
+        $text = explode('CCAA', $text);
+        $text = explode('IA:', $text[1]);
+        $this->comunidades_autonomas = preg_replace(['/	 	/', '/ 	/', '/	/'], [' ', ' ', ''], $text[0]);
     }
 
     public function getCases( String $type = 'hint', $cm_autonoma = null){
-        $casos_totales = explode(' ', preg_replace(['/\s\s\([0-9,]+\)/', '/[0-9]{2}[,][0-9]+/'], [''], rtrim($this->comunidades_autonomas[0])));
-        $datos = [];
-        if(in_array($type, ['general', 'all', 'ca'])) {
-            if($type == 'general' || $type == 'all') {
-                $data = [
-                    'casos_totales' => (float) str_replace('.', '', $casos_totales[0]), 
-                    'casos_nuevos' => (float) str_replace('.', '', $casos_totales[1]),
-                    'criticos' => (float) $casos_totales[3],
-                    'fallecidos' => (float) $casos_totales[4]
-                ];
-                if($type == 'all') {
-                    $datos[]['datos_globales'] = $data;
-                } else {
-                    $datos = $data;
-                }
-            }
+      
+            $result = preg_split("/[\n]/", $this->comunidades_autonomas);
+            $ccaa_array = [];
+            if(in_array($type, ['general', 'all', 'ca'])) {
+                foreach ($result as $data) {
+                    preg_match('/(?P<ccaa>[A-Za-záéíúóñ. -]+)(?:\s)?(?P<casos>[0-9.]+)(?:\s)?(?P<ia>[0-9,.]+)(?:\s)?(?P<uci>[0-9.,]+)(?:\s)?(?P<fallecidos>[0-9,.]+)/', $data, $result);
+                    if(!empty($result['ccaa'])) {
 
-            foreach(preg_split("/[\n]/", preg_replace('/[\t]/', '', $this->comunidades_autonomas[1])) as $dato) {
-                preg_match('/(?P<ccaa>[\wáéíóú.]+(?:[\Dáéíóú.]+)?)(?P<casos>[0-9]+)\s(?P<fallecidos>[0-9]+)/', preg_replace('/ 	/', '', $dato), $resultados);
-                if($type == 'ca' || $type == 'all') {
-                    if(!empty($resultados['ccaa'])) {
-                        if(strpos(strtolower($cm_autonoma), strtolower(rtrim($resultados['ccaa']))) !== FALSE && !empty($cm_autonoma)) {
-                            $datos[] = [
-                                'comunidad_autonoma' => rtrim($resultados['ccaa']),
-                                'casos' => (int) $resultados['casos'],
-                                'fallecidos' => (int) $resultados['fallecidos']
-                            ];
+                        $array = [
+                            'ccaa' => rtrim($result['ccaa']),
+                            'casos' => (float) $result['casos'],
+                            'ia' => (float) str_replace(',', '.', $result['ia']),
+                            'uci' => (float) $result['uci'],
+                            'fallecidos' => (float) $result['fallecidos']
+                        ];
+
+                        if(strpos(strtolower($cm_autonoma), strtolower(rtrim($result['ccaa']))) !== FALSE && !empty($cm_autonoma)) {
+                            $ccaa_array[] = $array;
+
                         } elseif (empty($cm_autonoma)) {
-                            $datos[] = [
-                                'comunidad_autonoma' => rtrim($resultados['ccaa']),
-                                'casos' => (int) $resultados['casos'],
-                                'fallecidos' => (int) $resultados['fallecidos']
-                            ];
+                            $ccaa_array[] = $array;
+
                         }
-                    } 
-                } 
+                    }
+                }
+            } elseif ($type == "hint") {
+                $ccaa_array = [
+                    [
+                        'nombre' => 'Obtener toda la información',
+                        'descripcion' => 'Obtener toda la información reciente sobre todas Comunidades autonomas de España',
+                        'url' => '/coronapi?type=',
+                        'parametros' => 'ca'
+                    ],
+                    [
+                        'nombre' => 'Información provincial',
+                        'descripcion' => 'Obtener toda la información reciente sobre Comunidades autonomas de España',
+                        'url' => '/coronapi?type=',
+                        'parametros' => 'ca',
+                        'sub_parametro' => '&ca={Comunidad Autonoma}'
+                    ],
+                    [
+                        'nombre' => 'Información provincial',
+                        'descripcion' => 'Obtener toda la información reciente sobre Comunidades autonomas de España',
+                        'url' => '/coronapi?type=',
+                        'parametros' => 'ca',
+                        'sub_parametro' => '&ca=TOTAL'
+                    ],
+                ];
             }
-        } elseif ($type == "hint") {
-            $datos = [
-                [
-                    'nombre' => 'Obtener toda la información',
-                    'descripcion' => 'Obtener toda la información reciente sobre todas Comunidades autonomas de España',
-                    'url' => '/coronapi?type=',
-                    'parametros' => 'all'
-                ],
-                [
-                    'nombre' => 'Información provincial',
-                    'descripcion' => 'Obtener toda la información reciente sobre Comunidades autonomas de España',
-                    'url' => '/coronapi?type=',
-                    'parametros' => 'ca',
-                    'sub_parametro' => '&ca={Comunidad Autonoma}'
-                ],
-                [
-                    'nombre' => 'Información general',
-                    'descripcion' => 'Obtener datos generales de todas las comunidades',
-                    'url' => '/coronapi?type=',
-                    'parametros' => 'general'
-                ],
-            ];
-        }
-        return json_encode($datos);
+        return json_encode($ccaa_array);
     }
 }
-
-
