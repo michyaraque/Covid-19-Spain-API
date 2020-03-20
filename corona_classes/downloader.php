6e2971b36627f7db67a97f5a4c0c0e8ca98c9e05
Michael Araque <32944677+michydev@users.noreply.github.com>
2020-03-20 22:33:53 +0100

    Update downloader.php

--- corona_classes/downloader.php
+++ corona_classes/downloader.php
@@ -1,16 +1,49 @@
 <?php
 
-class Download {
+/**
+* Clase creada para poder obtener el archivo PDF que es actualizado diariamente por el Ministerio de Salud
+* @author Michael Araque
+*/
 
-    public $data = null;
+class Download {
+    
+    /**
+     * @access public
+     * @var dir_path
+     */
+
     public $path = __DIR__ .'/data.pdf';
+    
+     /**
+     * @access public
+     * @var string
+     * Url de donde se encuentra la url principal para la descarga
+     */
+    
     public $download_url = 'https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/documentos/';
+    
+     /**
+     * @access public
+     * @var string
+     * Url donde se encuentra el contenido principal dado por el Ministerio de Salud
+     */
+    
     public $principal_url = 'https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/situacionActual.htm';
 
+    /**
+    * La función inicial __construct realiza la primera llamada para obtener el enlace de descarga
+    */
+    
     public function __construct() {
         $this->request($this->principal_url);
     }
 
+     /**
+     * @access public
+     * @param string
+     * @return CURL
+     */
+    
     public function request($url) {
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -21,11 +54,21 @@ class Download {
         curl_close($ch);
     }
 
+    /**
+     * @access public
+     * @return Url de descarga directa del archivo PDF
+     */
+    
     public function getDownloadUrl(){
         preg_match('/alertasActual\/nCov-China\/documentos\/(?P<name>[^.]+)/', $this->data, $result);
         return sprintf("%s%s.pdf",$this->download_url, $result['name']);;
     }
 
+    /**
+     * @access public
+     * @return Retorna el archivo ya guardado en la carpeta especificada
+     */
+    
     public function saveFile() { 
         try {
             $result = file_put_contents($this->path, $this->data);
