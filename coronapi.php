ddf790cab03fae827d042be075ecc3264d98cb55
Michael Araque <32944677+michydev@users.noreply.github.com>
2020-03-20 21:38:24 +0100

    Update coronapi.php

--- coronapi.php
+++ coronapi.php
@@ -6,7 +6,7 @@ $coronapi = new Coronavirus;
 if(!empty($_GET['type']) && in_array($_GET['type'], ['general', 'all'])) {
     $type = $_GET['type'];
     $cm_autonoma = null;
-} elseif ($_GET['type'] == 'ca') {
+} elseif (!empty($_GET['type']) && $_GET['type'] == 'ca') {
     $type = "ca";
     if(!empty($_GET['ca']) && is_string($_GET['ca'])) {
         $cm_autonoma = $_GET['ca'];
@@ -17,4 +17,5 @@ if(!empty($_GET['type']) && in_array($_GET['type'], ['general', 'all'])) {
     $type = 'hint';
     $cm_autonoma = null;
 }
+
 echo $coronapi->getCases($type, $cm_autonoma);
