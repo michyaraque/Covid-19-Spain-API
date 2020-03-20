a1d4b4630899e0a77ca31f0421887e41ec3e1432
Michael Araque <32944677+michydev@users.noreply.github.com>
2020-03-20 22:36:59 +0100

    Update utils.php

--- corona_classes/utils.php
+++ corona_classes/utils.php
@@ -1,11 +1,16 @@
 <?php
 
+/**
+* @author Michael Araque
+*/
 class Utils {
 
     /**
-     * [STRING]     $value
-     * [RETURN]     format number
+     * @access     public
+     * @var     $value Valor número o dato número
+     * @return Valor formateado sin "." o ","
      */
+    
     public static function format_n ($value) {
         if(str_replace('.', '', $value) == TRUE) {
             $value = str_replace('.', '', $value);
