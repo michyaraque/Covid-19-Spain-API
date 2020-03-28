4c3f11ffafaa54064b8814337019067661956075
Michael Araque <32944677+michydev@users.noreply.github.com>
2020-03-28 17:29:58 +0100

    Missing name functions

--- src/utils.php
+++ src/utils.php
@@ -1,80 +1,38 @@
 <?php
 
 /**
-* @author Michael Araque
-*/
+ * @author Michael Araque
+ */
 
-class Utils {
+class CovidHandler {
 
     /**
      * @access public
-     * @var string
+     * @param $http_code Código relacionado a la petición
+     * @param $msg Mensaje que se enviará en formato de json
+     * @return object Se retorna un json con los datos formateados
+     */
+
+    public static function createReply($http_code, $msg) {
+        return Utils::print_obj([
+            'code' => $http_code,
+            'description' => $msg
+            ]);
+    }
+
+    /**
+     * @return object ///
+     */
+
+    public static function invalidObject() {
+        return self::createReply(204, 'missing parameter');
+    }
+
+    /**
+     * @return object ///
      */
     
-    public static $path = __DIR__ .'/../data.pdf';
-
-    /**
-     * @access public
-     * @var string Palabra a cambiar
-     * @return string Palabra en minúscula y sin posibles espacios a la derecha
-     */
-
-    public static function str_lowerise($string) {
-        return strtolower(rtrim($string));
-    }
-
-    /**
-     * @access public
-     * @var     $value Valor número o dato número
-     * @return Valor formateado sin "." o ","
-     */
-    
-    public static function format_n ($value) {
-        if(str_replace('.', '', $value) == TRUE) {
-            $value = str_replace('.', '', $value);
-            return (integer) $value;
-        } else {
-            return 0;
-        }
-    }
-
-    /**
-     * @access public
-     * @return Fecha de la última modificación del archivo PDF
-     */
-
-    public static function getLastModifiedFile() {
-        if (file_exists(self::$path)) {
-            return date("d/m/Y H:i:s", filectime(self::$path));
-        }
-    }
-
-     /**
-     * @access public
-     * @return string Devuelve los datos parseados de cada línea de la tabla del PDF
-     */
-
-    public static function str_parsereg($data) {
-        preg_match('/(?P<ccaa>[A-Za-záéíúóñÑ. -]+)(?:\s)?(?P<casos>[0-9.]+)(?:\s)?(?P<ia>[0-9,.]+)(?:\s)?(?P<hospitalizados>[0-9.,]+)(?:\s)?(?P<uci>[0-9,.]+)(?:\s(?P<fallecidos>[0-9.,¥]+)\s(?P<curados>[0-9.,]+)\s(?P<new_cases>[0-9.,]+))?/', $data, $result);
-        return $result;
-    }
-
-     /**
-     * @access public
-     * @param @data Objeto que se envia en la petición de la función
-     * @return object Devuelve el json en formato legible
-     */
-
-    public static function print_obj($data) {
-        try {
-            if(!empty($data)) {
-                return json_encode($data, JSON_PRETTY_PRINT);
-            } else {
-                throw new Exception('Object not included in the data parameter');
-            }
-        } catch (Exception $e) {
-            echo '[COVID-19] '. $e->getMessage();
-        }
-        
+    public static function invalidRequest() {
+        return self::createReply(200, 'empty request');   
     }
 }
