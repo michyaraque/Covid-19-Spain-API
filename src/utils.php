13988b4c08f24296e37f18908b4780a35b247eeb
Michael Araque <32944677+michydev@users.noreply.github.com>
2020-03-28 16:26:50 +0100

    Regex arreglado añadido simbolo (¥)
    Se ha añadido excepción a este símbolo debido a que el PDF del Ministerio de Sanidad ha añadido por error en ciertos elementos este símbolo

--- src/utils.php
+++ src/utils.php
@@ -55,7 +55,7 @@ class Utils {
      */
 
     public static function str_parsereg($data) {
-        preg_match('/(?P<ccaa>[A-Za-záéíúóñÑ. -]+)(?:\s)?(?P<casos>[0-9.]+)(?:\s)?(?P<ia>[0-9,.]+)(?:\s)?(?P<hospitalizados>[0-9.,]+)(?:\s)?(?P<uci>[0-9,.]+)(?:\s(?P<fallecidos>[0-9.,]+)\s(?P<curados>[0-9.,]+)\s(?P<new_cases>[0-9.,]+))?/', $data, $result);
+        preg_match('/(?P<ccaa>[A-Za-záéíúóñÑ. -]+)(?:\s)?(?P<casos>[0-9.]+)(?:\s)?(?P<ia>[0-9,.]+)(?:\s)?(?P<hospitalizados>[0-9.,]+)(?:\s)?(?P<uci>[0-9,.]+)(?:\s(?P<fallecidos>[0-9.,¥]+)\s(?P<curados>[0-9.,]+)\s(?P<new_cases>[0-9.,]+))?/', $data, $result);
         return $result;
     }
 
